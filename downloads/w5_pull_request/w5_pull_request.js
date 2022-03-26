505223f985dd7742b6d94427d930c58b9bda4187
mdecourse <mdecourse@gmail.com>
2022-03-26 21:28:13 +0800

    add pull requests Wink tutorial

--- downloads/w5_pull_request/w5_pull_request.js
+++ downloads/w5_pull_request/w5_pull_request.js
@@ -4,5 +4,8 @@
   buttonFrameLength: 5,
   buttonFrameOffset: 2,
   frameStops: {
+    "1682": [
+      { type: "gotourl", rect: { x: 321, y: 166, width: 60, height: 24 }, target: "https://support.mozilla.org/en-US/questions/1230877", window: "reset certificate" },
+    ],
   },
 };
